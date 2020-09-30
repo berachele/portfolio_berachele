75790d57fb64bf4a1e27da0436aae9aaa0c9929c
Rachele Edwards <dot.com1472@gmail.com>
2020-09-30 13:49:58 -0600

    added simple formatting for components to render

--- portfolio_berachele/src/components/pages/HomePage.js
+++ portfolio_berachele/src/components/pages/HomePage.js
@@ -1,11 +1,23 @@
 import React from 'react';
+//Styles
 import '../../styles/HomePage.css'
+//Components
+import NavBar from './NavBar'
+import Welcome from './Welcome'
+import Skills from './Skills'
+import Projects from './Projects'
+import Contact from './Contact'
 
 function HomePage() {
   return (
     <div className="HomePage">
       <header className="HomePage-header">
         <h1>Hello, World!</h1>
+        <NavBar />
+        <Welcome />
+        <Skills />
+        <Projects />
+        <Contact />
       </header>
     </div>
   );
---
+++ portfolio_berachele/src/components/pages/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function NavBar() {
+
+
+    return (
+        <h2>Hello from NavBar</h2>
+    )
+}
