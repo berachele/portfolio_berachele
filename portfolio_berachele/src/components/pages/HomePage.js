f8eb85f9d76db9b98cf51179cf1713c55bd5ff78
Rachele Edwards <dot.com1472@gmail.com>
2020-09-30 12:02:37 -0600

    added folders to components for organization

---
+++ portfolio_berachele/src/components/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../styles/HomePage.css'
+
+function HomePage() {
+  return (
+    <div className="HomePage">
+      <header className="HomePage-header">
+        <h1>Hello, World!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default HomePage;
