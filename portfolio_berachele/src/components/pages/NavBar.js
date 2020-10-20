55821c36a40a94fc3939e09d6ecdad053f47f018
Rachele Edwards <dot.com1472@gmail.com>
2020-10-20 12:09:05 -0600

    added antd dividers but isn't visible, trying to change color and can't see

--- portfolio_berachele/src/components/pages/NavBar.js
+++ portfolio_berachele/src/components/pages/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Divider } from 'antd';
 
 export default function NavBar() {
 
@@ -6,9 +7,13 @@ export default function NavBar() {
     return (
         <div className='nav-bar'>
             <h3>Welcome</h3>
+            <Divider style={{color: 'red'}}/>
             <h3>What I Do</h3>
+            <Divider className='divider'/>
             <h3>Projects</h3>
+            <Divider />
             <h3>Get in Touch</h3>
+            <Divider />
         </div>
     )
 }
