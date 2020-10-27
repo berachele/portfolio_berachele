ca5909231641584747470fb3b7773aa57f0a9a90
Rachele Edwards <dot.com1472@gmail.com>
2020-10-27 17:19:01 -0600

    changed less files to scss and got styles to work

--- portfolio_berachele/src/index.js
+++ portfolio_berachele/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/styles.css'
+import './styles/styles.scss'
 import HomePage from './components/pages/HomePage';
 import * as serviceWorker from './serviceWorker';
 
